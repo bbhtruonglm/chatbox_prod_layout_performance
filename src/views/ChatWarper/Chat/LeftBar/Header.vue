1618e0618af0d5e0759658dfa27831861e20be4d
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-04 15:56:44 +0700

    temp: comment QuickFilter for testing

--- src/views/ChatWarper/Chat/LeftBar/Header.vue
+++ src/views/ChatWarper/Chat/LeftBar/Header.vue
@@ -127,7 +127,7 @@
       <XMarkIcon class="w-3.5 h-3.5 flex-shrink-0" />
     </button>
   </div>
-  <QuickFilter />
+  <!-- <QuickFilter /> -->
 </template>
 <script setup lang="ts">
 import { isFilterActive } from '@/service/function'
