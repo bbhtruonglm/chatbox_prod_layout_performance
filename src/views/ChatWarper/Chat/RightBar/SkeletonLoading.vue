d67e8a0b054e2eb47a932c310a5f9c38789974d2
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-07 14:40:01 +0700

    update

--- src/views/ChatWarper/Chat/RightBar/SkeletonLoading.vue
+++ src/views/ChatWarper/Chat/RightBar/SkeletonLoading.vue
@@ -5,18 +5,26 @@
     <div
       v-for="i in 3"
       :key="i"
-      class="rounded-lg bg-white overflow-hidden flex-shrink-0 flex flex-col border border-slate-100"
+      class="rounded-lg bg-white overflow-hidden flex-shrink-0 flex flex-col border-none border-slate-100"
+      :class="{
+        'flex-grow': i === 1,
+      }"
     >
       <div
-        class="w-full py-2.5 px-3 flex items-center justify-between bg-white border-b border-slate-50"
+        class="w-full py-2.5 px-3 flex items-center justify-between bg-white"
+        :class="{
+          'border-b border-slate-50': i === 1,
+        }"
       >
-        <div class="w-32 h-4 bg-slate-200 rounded-full animate-pulse"></div>
+        <div class="w-32 h-4 bg-slate-200 rounded animate-pulse"></div>
         <div class="flex gap-3">
-          <div class="w-5 h-5 bg-slate-200 rounded-full animate-pulse"></div>
-          <div class="w-4 h-4 bg-slate-200 rounded-full animate-pulse"></div>
+          <div class="size-8 bg-slate-200 rounded animate-pulse"></div>
         </div>
       </div>
-      <div class="w-full h-[300px] bg-slate-50 animate-pulse"></div>
+      <div
+        v-if="i === 1"
+        class="w-full flex-grow bg-slate-50 animate-pulse"
+      ></div>
     </div>
   </div>
 </template>
